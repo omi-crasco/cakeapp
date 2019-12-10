4d968782f8b2ca63e237a13023970fd41e0da92b
oumi.crasco <oumi@crasco.jp>
2019-12-10 12:39:54 +0900

    スラグ生成まで

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,11 +3,22 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// Text クラス
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
