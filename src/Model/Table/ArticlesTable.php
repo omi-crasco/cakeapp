22aba2d36a7c1dbc48bdbe14a644b1677e6db5b1
oumi.crasco <oumi@crasco.jp>
2019-12-10 14:04:31 +0900

    検証追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,6 +2,7 @@
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+use Cake\Validation\Validator;
 use Cake\ORM\Table;
 // Text クラス
 use Cake\Utility\Text;
@@ -20,5 +21,18 @@ class ArticlesTable extends Table
             // スラグをスキーマで定義されている最大長に調整
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
+    }
+
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmptyString('title', false)
+            ->minLength('title', 1)
+            ->maxLength('title', 255)
+
+            ->allowEmptyString('body', false)
+            ->minLength('body', 1);
+
+        return $validator;
     }
 }
